9b338488e3ee3290a7e4e3b84bc1f671ea917bd5
Emma Harper Smith <emma@emmatyping.dev>
2026-03-03 01:13:49 +0000

    Pass through C standard flag to libclang for sanitizers

--- Modules/cpython-sys/build.rs
+++ Modules/cpython-sys/build.rs
@@ -76,6 +76,7 @@ fn generate_c_api_bindings(srcdir: &Path, builddir: Option<&str>, out_path: &Pat
                     }
                 } else if flag.starts_with("-I")
                     || flag.starts_with("-D")
+                    || flag.starts_with("-std=")
                     || flag.starts_with("-isystem")
                 {
                     builder = builder.clang_arg(flag);
