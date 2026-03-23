bb7b9bb2a6cf256ed52be0b189a8d09df2636b5b
Kirill Podoprigora <kirill.bast9@mail.ru>
2026-03-24 01:05:30 +0200

    hm

--- Modules/cpython-sys/wrapper.h
+++ Modules/cpython-sys/wrapper.h
@@ -32,11 +32,6 @@
 #define Py_BUILD_CORE
 
 // Internal
-// Ensure C11 atomics are available for mimalloc headers.
-// Clang supports <stdatomic.h> in all C modes via __has_extension(c_atomic).
-#if !defined(__cplusplus)
-#include <stdatomic.h>
-#endif
 #include "internal/pycore_parser.h"
 #include "internal/pycore_mimalloc.h"
 #include "internal/mimalloc/mimalloc.h"
